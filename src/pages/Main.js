3974b46a4ba7a2df7d5cc26a68f5b355bda59999
jjiinaaa <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>
2024-11-18 08:35:02 +0000

    Create sustain4u.yml

---
+++ src/pages/Main.js
@@ -0,0 +1,189 @@
+import React from "react";
+import styled from "styled-components";
+import { Tobbar } from "../components/Tobbar";
+import Arrow from "../assets/image/arrow.svg";
+import { Link } from "react-router-dom";
+
+const Container = styled.div`
+  height: 82vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media screen and (max-width: 767px) {
+    width: 82%;
+    margin-top: 10%;
+    padding: 0 9%;
+  }
+  @media screen and (min-width: 768px) {
+    width: 70%;
+    margin-top: 2%;
+    padding: 0 15%;
+  }
+`;
+
+const MessageContainer = styled.div`
+  width: 100%;
+  @media screen and (max-width: 767px) {
+    margin: 0 0 6%;
+  }
+  @media screen and (min-width: 768px) {
+    margin: 0 0 3%;
+  }
+`;
+
+const WelcomeText = styled.h1`
+  font-family: Pretendard;
+  font-weight: 900;
+  color: #5c5b5b;
+  @media screen and (max-width: 767px) {
+    font-size: 1rem;
+    margin: 0 0 10px;
+    padding-left: 6%;
+  }
+  @media screen and (min-width: 768px) {
+    font-size: 1.5rem;
+    margin: 0 0 15px;
+    padding-left: 8%;
+  }
+`;
+
+const Subtitle = styled.p`
+  font-family: Pretendard;
+  font-weight: 300;
+  color: #5c5b5b;
+  margin: 0;
+  @media screen and (max-width: 767px) {
+    font-size: 0.65rem;
+    padding-left: 6%;
+  }
+  @media screen and (min-width: 768px) {
+    font-size: 0.8rem;
+    padding-left: 8%;
+  }
+`;
+
+const Card = styled(Link)`
+  width: 80%;
+  background-color: #ffffff;
+  padding: 30px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  cursor: pointer;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.1);
+  text-align: left;
+  transition: background-color 0.3s ease;
+  position: relative;
+  text-decoration: none;
+
+  @media screen and (max-width: 767px) {
+    margin: 0 0 5%;
+  }
+  @media screen and (min-width: 768px) {
+    margin: 0 0 2%;
+  }
+
+  &:hover {
+    background-color: #005bac;
+
+    h2,
+    p {
+      color: white;
+    }
+  }
+`;
+
+const CardTitle = styled.h2`
+  font-size: 1rem;
+  font-weight: bold;
+  color: #5c5b5b;
+  transition: color 0.3s ease;
+`;
+
+const CardSubtitle = styled.p`
+  font-size: 0.67rem;
+  color: gray;
+  margin-top: 1px;
+  transition: color 0.3s ease;
+`;
+
+const ArrowIconTurnBox = styled.div`
+  cursor: pointer;
+  position: absolute;
+  left: 90%;
+  top: 35%;
+  width: 13px;
+  height: 22px;
+  transform: translateY(50%);
+`;
+
+const ArrowIconTurn = styled.img`
+  width: 70%;
+  height: 80%;
+  transform: rotate(180deg);
+`;
+
+const Footer = styled.footer`
+  width: 100%;
+  height: 10vh;
+  display: fixed;
+  bottom: 0;
+  background-color: #ffffff;
+  color: #5c5b5b;
+  box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.2);
+`;
+
+function Main() {
+  return (
+    <>
+      <Tobbar content='MAIN' />
+      <Container>
+        <MessageContainer>
+          <WelcomeText>Welcome, USER3124</WelcomeText>
+          <Subtitle>
+            Part-time notification service <br />
+            for Inha University international students
+          </Subtitle>
+        </MessageContainer>
+
+        <Card to='/checklist1'>
+          <ArrowIconTurnBox>
+            <ArrowIconTurn src={Arrow} alt='go' />
+          </ArrowIconTurnBox>
+          <CardTitle>
+            Check permitted
+            <br />
+            Working hours
+          </CardTitle>
+          <CardSubtitle>근로 허용 시간 확인</CardSubtitle>
+        </Card>
+
+        <Card
+          onClick={() => console.log("Navigate to Unpermitted Work Categories")}
+        >
+          <ArrowIconTurnBox>
+            <ArrowIconTurn src={Arrow} alt='go' />
+          </ArrowIconTurnBox>
+          <CardTitle>
+            Unpermitted
+            <br />
+            Work Categories
+          </CardTitle>
+          <CardSubtitle>국제학생 시간제 취업 불허 직종</CardSubtitle>
+        </Card>
+
+        <Card onClick={() => console.log("Navigate to Employment Contract")}>
+          <ArrowIconTurnBox>
+            <ArrowIconTurn src={Arrow} alt='go' />
+          </ArrowIconTurnBox>
+          <CardTitle>Employment Contract</CardTitle>
+          <CardSubtitle>근로계약서 출력</CardSubtitle>
+        </Card>
+      </Container>
+      <Footer />
+    </>
+  );
+}
+
+export default Main;
